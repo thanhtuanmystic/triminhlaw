de1a79edd0281ce28299f3ef3ba5f551af321b0e
thanhtuanmystic <tuanvp2001@gmail.com>
2024-08-27 09:47:09 +0700

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,58 @@
+// Header
+// search-box open close js code
+let navbar = document.querySelector(".navbar");
+let searchBox = document.querySelector(".search-box .bx-search");
+// let searchBoxCancel = document.querySelector(".search-box .bx-x");
+
+// sidebar open close js code
+let navLinks = document.querySelector(".nav-links");
+let menuOpenBtn = document.querySelector(".navbar .bx-menu");
+let menuCloseBtn = document.querySelector(".nav-links .bx-x");
+menuOpenBtn.onclick = function () {
+  navLinks.style.left = "0";
+};
+menuCloseBtn.onclick = function () {
+  navLinks.style.left = "-100%";
+};
+
+// sidebar submenu open close js code
+// let htmlcssArrow = document.querySelector(".htmlcss-arrow");
+// htmlcssArrow.onclick = function () {
+//   navLinks.classList.toggle("show1");
+// };
+// let moreArrow = document.querySelector(".more-arrow");
+// moreArrow.onclick = function () {
+//   navLinks.classList.toggle("show2");
+// };
+// let jsArrow = document.querySelector(".js-arrow");
+// jsArrow.onclick = function () {
+//   navLinks.classList.toggle("show3");
+// };
+
+// Scroll Partner
+const scrollers = document.querySelectorAll(".scroller");
+
+// If an user hasn't opted in for recuded motion, then we add the animation
+if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
+  addAnimation();
+}
+
+function addAnimation() {
+  scrollers.forEach((scroller) => {
+    // add data-animated="true" to every `.scroller` on the page
+    scroller.setAttribute("data-animated", true);
+
+    // Make an array from the elements within `.scroller-inner`
+    const scrollerInner = scroller.querySelector(".scroller__inner");
+    const scrollerContent = Array.from(scrollerInner.children);
+
+    // For each item in the array, clone it
+    // add aria-hidden to it
+    // add it into the `.scroller-inner`
+    scrollerContent.forEach((item) => {
+      const duplicatedItem = item.cloneNode(true);
+      duplicatedItem.setAttribute("aria-hidden", true);
+      scrollerInner.appendChild(duplicatedItem);
+    });
+  });
+}
